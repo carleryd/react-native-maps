606d129a9e6bc5725917e99097280e3a2dc3f68d
Christofer Ärleryd <c.arleryd@gmail.com>
2016-11-16 14:14:48 +0100

    Merge location

--- ios/AirMaps/AIRMap.m
+++ ios/AirMaps/AIRMap.m
@@ -155,16 +155,7 @@ const CGFloat AIRMapZoomBoundBuffer = 0.01;
     UIView *calloutMaybe = [self.calloutView hitTest:[self.calloutView convertPoint:point fromView:self] withEvent:event];
     if (calloutMaybe) return calloutMaybe;
 
-    // If it's not a callout, then always return the MKNewAnnotationContainerView which will handle pinch & zoom properly
-    // - MKMapView
-    // - - UIView
-    // - - - MKBasicMapView
-    // - - - - _MKMapLayerHostingView
-    // - - - MKScrollContainerView
-    // - - - - MKOverlayContainerView
-    // - - - MKNewAnnotationContainerView
-    // - - MKAttributionLabel
-    return ((UIView *)[((UIView *)[self.subviews objectAtIndex:0]).subviews objectAtIndex:2]);
+    return [super hitTest:point withEvent:event];
 }
 
 #pragma mark SMCalloutViewDelegate
